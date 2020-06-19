abe43d0eab29e013c129e963b367a75fbb2b2e22
Eric <ericmuriel@hotmail.com>
2020-06-19 20:14:22 +0200

    solving responsive

--- src/components/Footer/SocialNet/SocialNet.js
+++ src/components/Footer/SocialNet/SocialNet.js
@@ -6,7 +6,7 @@ const SocialNet = ({ url, img }) => {
   return (
     <div className="socialnet">
       <a href={url} target="_blank" rel="noopener noreferrer">
-        <img src={img} width="60" alt=""></img>
+        <img src={img} width="60" className="imgsocialnet" alt=""></img>
       </a>
     </div>
   );
